41638a6067b6098727705d7b04fda1501cd3d26c
artificial-life <proteus.protocol@gmail.com>
2016-05-17 01:22:22 +0600

    upd

--- src/db/Slave.js
+++ src/db/Slave.js
@@ -6,31 +6,34 @@ let _buckets = {};
 let cb = false;
 
 function getMulti(bucket_name, keys, id) {
-	_buckets[bucket_name].getMulti(keys, (err, res) => process.send({
-		data: res,
-		err: err,
-		id: id
-	}));
+  _buckets[bucket_name].getMulti(keys, (err, res) => process.send({
+    data: res,
+    err: err,
+    id: id
+  }));
 }
 
 function config(cfg) {
-	cb = new Couchbase.Cluster(cfg.server_ip);
+  cb = new Couchbase.Cluster(cfg.server_ip);
 }
 
 function bucket(name) {
-	_buckets[name] = cb.openBucket(name);
+  _buckets[name] = cb.openBucket(name);
 }
 
 process.on('message', (m) => {
-	switch (m.type) {
-	case 'config':
-		config(m.data);
-		break;
-	case 'bucket':
-		bucket(m.data);
-		break;
-	case 'getMulti':
-		getMulti(m.bucket, m.data, m.request_id);
-		break;
-	}
+  switch (m.type) {
+    case 'config':
+      config(m.data);
+      break;
+    case 'bucket':
+      bucket(m.data);
+      break;
+    case 'kill':
+      process.exit();
+      break;
+    case 'getMulti':
+      getMulti(m.bucket, m.data, m.request_id);
+      break;
+  }
 });
